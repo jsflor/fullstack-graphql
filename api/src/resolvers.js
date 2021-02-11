be3db5088e3339991e8a6a4b16368e00bbeecf9b
Sebastian Flor <sflor@telecomboutique.com>
2021-02-11 12:49:43 +0100

    feat(): update schema & resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,31 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, { models }) {
+      return models.Pet.findMany(input);
+    },
+    pet(_, { input }, { models }) {
+      return models.Pet.findOne(input);
+    },
   },
   Mutation: {
-    
+    newPet(_, { input }, { models }) {
+      return models.Pet.create(input);
+    },
   },
   Pet: {
+    owner(_, __, { user }) {
+      return user;
+    },
     img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+      return pet.type === "DOG"
+        ? "https://placedog.net/300/300"
+        : "http://placekitten.com/300/300";
+    },
   },
   User: {
-    
-  }
-}
+    pets(_, __, { models }) {
+      return models.Pet.findMany();
+    },
+  },
+};
